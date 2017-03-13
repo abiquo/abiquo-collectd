ccd8be0a2d3b4551c2e300afa480258f51697564
Ignasi Barrera <nacx@apache.org>
2017-03-13 17:12:00 +0100

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,10 @@ source_url       'https://github.com/abiquo/collectd-abiquo-cookbook'
 issues_url       'https://github.com/abiquo/collectd-abiquo-cookbook/issues'
 version          '0.2.0'
 
+recipe 'collectd-abiquo', 'Installs collectd and the Abiquo monitoring plugin'
+recipe 'collectd-abiquo::collectd', 'Installs and configures collectd and the default plugins'
+recipe 'collectd-abiquo::plugin', 'Installs and configures the Abiquo collectd plugin'
+
 supports 'centos', '~> 6.7'
 supports 'ubuntu', '~> 16.04'
 
